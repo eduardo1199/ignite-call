78680dc0a536f056bcd661eb08c63c8637cfe3b1
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-05-03 09:53:37 -0300

    settings main app config

---
+++ src/pages/index.page.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+import { Heading } from '@ignite-ui/react'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Heading as="h1">Hello word</Heading>
+    </div>
+  )
+}
+
+export default Home
