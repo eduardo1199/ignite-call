a56849c4481b084dd1fb6859c489539a5d229d35
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-06-02 12:19:35 -0300

    feat: setting next auth authentication for google

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -2,12 +2,9 @@ import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
 import { Container, Header } from '../styles'
 import { ArrowRight } from 'phosphor-react'
 import { ConnectBox, ConnectItem } from './styles'
+import { signIn } from 'next-auth/react'
 
 export default function Register() {
-  /* async function handleRegister(data: RegisterFormData) {
-    
-  } */
-
   return (
     <Container>
       <Header>
@@ -22,7 +19,7 @@ export default function Register() {
         <ConnectBox>
           <ConnectItem>
             <Text>Google Calendar</Text>
-            <Button variant="secondary">
+            <Button variant="secondary" onClick={() => signIn('google')}>
               Conectar <ArrowRight />
             </Button>
           </ConnectItem>
