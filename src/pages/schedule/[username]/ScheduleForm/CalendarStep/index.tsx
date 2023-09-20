92457500441fb8c0dd815f1813d2adf695cb1d7d
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-09-20 10:54:03 -0300

    feat: format date table hours

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -7,11 +7,17 @@ import {
   TimePickerList,
 } from './styles'
 import { useState } from 'react'
+import dayjs from 'dayjs'
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
 
   const isDateSelected = !!selectedDate
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const currentDateLabel = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null
 
   function handleOnSelectedDate(date: Date) {
     setSelectedDate(date)
@@ -27,7 +33,7 @@ export function CalendarStep() {
       {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
-            terça-feira <span>20 de setembro</span>
+            {weekDay} <span>{currentDateLabel}</span>
           </TimePickerHeader>
 
           <TimePickerList>
