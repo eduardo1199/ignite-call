fc9f62f073d9f1fdf6676bc1caae54a5a2d28d37
Eduardo Soares de Araujo Aquino <eduardo.soares.116@ufrn.edu.br>
2023-11-04 10:08:23 -0300

    feat: migrate database sqlite to mysql with docker compose

--- src/pages/api/users/[username]/blocked-date.api.ts
+++ src/pages/api/users/[username]/blocked-date.api.ts
@@ -40,5 +40,13 @@ export default async function handle(
     )
   })
 
-  return res.json({ blockedWeekDays })
+  const blockedDaysRow = await prisma.$queryRaw`
+    SELECT * 
+    FROM scheduling S
+
+    WHERE S.user_id = ${user.id}
+      AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
+  `
+
+  return res.json({ blockedWeekDays, blockedDaysRow })
 }
